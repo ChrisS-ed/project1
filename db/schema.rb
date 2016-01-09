766ac410851375f724aa16cde88880b66eed0d92
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-09 16:30:01 +0000

    created Booking model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109162534) do
+ActiveRecord::Schema.define(version: 20160109162837) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160109162534) do
     t.text     "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "gig_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "gigs", force: :cascade do |t|
