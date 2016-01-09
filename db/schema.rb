d5302e58719eeacf855c287188d2ee5ccad652a1
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-09 16:23:28 +0000

    added Venue model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109161638) do
+ActiveRecord::Schema.define(version: 20160109162048) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -38,5 +38,13 @@ ActiveRecord::Schema.define(version: 20160109161638) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "venues", force: :cascade do |t|
+    t.string   "name"
+    t.string   "location"
+    t.text     "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
