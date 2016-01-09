d50f1bb03bc146a9386fd8bfdeedd5a18d99e703
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-09 16:19:50 +0000

    created Artist model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109115825) do
+ActiveRecord::Schema.define(version: 20160109161638) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.text     "image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
