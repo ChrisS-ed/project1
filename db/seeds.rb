30848fe80b6b55dca7fd2db7fa65470572e33aa0
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-10 11:13:50 +0000

    added seeds for venues

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,13 @@ Artist.create!(name: 'Ed Eales-White and Jon Pointing', description: 'Writers an
 Artist.create!(name: 'Babolin Theatre', description: 'Cambridge-based professional theatre company for young people.', image: 'blank')
 Artist.create!(name: 'Knuckle and Joint', description: 'Puppetry company based in Canterbury.', image: 'blank')
 Artist.create!(name: 'Royal Over-Seas League', description: 'Award-winning musicians including Charlotte Ashton flute, Solem Quartet, Zelkova Quartet, Quattro (cello quartet), Françoise-Green piano duo, pianists Jan Hugo and Alexander Soares.', image: 'blank')
+
+Venue.delete_all
+
+Venue.create!(name: 'Pleasance Dome', location: 'Potterrow, EH8 9AL', image: 'blank')
+Venue.create!(name: 'Pleasance Courtyard', location: '60 Pleasance, EH8 9TJ', image: 'blank')
+Venue.create!(name: 'Bedlam Theatre', location: '11b Bristo Place, EH1 1EZ', image: 'blank')
+Venue.create!(name: 'Surgeons Hall', location: 'Nicolson Street, EH8 9DW', image: 'blank')
+Venue.create!(name: 'Royal Over-Seas League', location: 'Over-Seas House, EH2 3AB', image: 'blank')
+
+
