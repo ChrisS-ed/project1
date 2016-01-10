23469a78bf9b15c9898ae61813d8152ed160f029
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-10 19:43:58 +0000

    added seeds for user and booking

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,10 +24,18 @@ v5 = Venue.create!(name: 'Royal Over-Seas League', location: 'Over-Seas House, E
 
 Gig.delete_all
 
-Gig.create!(name: 'Double Act', description: 'Lazy Susan stand side by side, sling on some wigs and take a few long shots at triumph. Watch as they make pacts, shake hands and get drunk on fiction. Character comedy that shoots straight from the hip: come get a slice of the zeal.', artist_id: a1.id, venue_id: v1.id, tickets_available: 100, start_time: '2016-08-31 20:10:00', end_time: '2016-08-31 21:10:00')
-Gig.create!(name: 'Bucket', description: 'Ed Eales-White and Jon Pointing slam down a new character/sketch/double-act comedy show.', artist_id: a2.id, venue_id: v2.id, tickets_available: 50, start_time: '2016-08-30 18:00:00', end_time: '2016-08-30 19:00:00')
-Gig.create!(name: 'The Frantic Canticles of Little Brother Fish', description: 'Babolin welcome you to their monastery in deepest Depravia, where choristers chant and goose quills frantically scratch out the chronicle of an ancient quest.', artist_id: a3.id, venue_id: v3.id, tickets_available: 300, start_time: '2016-08-22 13:30:00', end_time: '2016-08-22 14:30:00')
-Gig.create!(name: 'The Black Hoods Cabaret', description: 'Bear witness to the debauchery and murder of some of the most decrepit puppets to crawl out of a back alley. A puppet cabaret for adults that will charm you into darkness and mayhem, as a pair of calamitous puppeteers manipulate a variety of disturbed and troubled puppets.', artist_id: a4.id, venue_id: v4.id, tickets_available: 150, start_time: '2016-08-22 22:10:00', end_time: '2016-08-22 23:10:00')
-Gig.create!(name: 'Mozart at Teatime', description: 'Different artists and programmes each concert. Tea/coffee shortbread afterwards included.', artist_id: a5.id, venue_id: v5.id, tickets_available: 100, start_time: '2016-08-19 16:00:00', end_time: '2016-08-19 17:00:00')
+g1 = Gig.create!(name: 'Double Act', description: 'Lazy Susan stand side by side, sling on some wigs and take a few long shots at triumph. Watch as they make pacts, shake hands and get drunk on fiction. Character comedy that shoots straight from the hip: come get a slice of the zeal.', artist_id: a1.id, venue_id: v1.id, tickets_available: 100, start_time: '2016-08-31 20:10:00', end_time: '2016-08-31 21:10:00')
+g2 = Gig.create!(name: 'Bucket', description: 'Ed Eales-White and Jon Pointing slam down a new character/sketch/double-act comedy show.', artist_id: a2.id, venue_id: v2.id, tickets_available: 50, start_time: '2016-08-30 18:00:00', end_time: '2016-08-30 19:00:00')
+g3 = Gig.create!(name: 'The Frantic Canticles of Little Brother Fish', description: 'Babolin welcome you to their monastery in deepest Depravia, where choristers chant and goose quills frantically scratch out the chronicle of an ancient quest.', artist_id: a3.id, venue_id: v3.id, tickets_available: 300, start_time: '2016-08-22 13:30:00', end_time: '2016-08-22 14:30:00')
+g4 = Gig.create!(name: 'The Black Hoods Cabaret', description: 'Bear witness to the debauchery and murder of some of the most decrepit puppets to crawl out of a back alley. A puppet cabaret for adults that will charm you into darkness and mayhem, as a pair of calamitous puppeteers manipulate a variety of disturbed and troubled puppets.', artist_id: a4.id, venue_id: v4.id, tickets_available: 150, start_time: '2016-08-22 22:10:00', end_time: '2016-08-22 23:10:00')
+g5 = Gig.create!(name: 'Mozart at Teatime', description: 'Different artists and programmes each concert. Tea/coffee shortbread afterwards included.', artist_id: a5.id, venue_id: v5.id, tickets_available: 100, start_time: '2016-08-19 16:00:00', end_time: '2016-08-19 17:00:00')
+
+User.delete_all
+
+u1 = User.create!(email: 'test@email.com', password: '12345678')
+
+Booking.delete_all
+
+Booking.create!(seats: 2, user_id: u1.id, gig_id: g1.id)
 
 
