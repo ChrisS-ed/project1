5667440a614097769056b731155c1b06eadb416e
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-10 18:46:53 +0000

    added associations for user, booking and gig

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,7 @@
 class BookingsController < ApplicationController
     
+  def index
+    @bookings = Booking.all
+  end
+
 end
--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,4 +1,7 @@
 class Gig < ActiveRecord::Base
   belongs_to :artist
   belongs_to :venue
+
+  has_many :bookings
+  has_many :users, through: :bookings
 end
