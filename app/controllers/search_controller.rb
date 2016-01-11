93da8f3db4fa5f7d9877ba9ee00826e86f9b4188
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-11 17:15:19 +0000

    search working for artists

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,8 @@
 class SearchController < ApplicationController
+
+  def index
+    artists = Artist.search(params[:search])
+    @results = artists    
+  end
+
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,9 @@
 class Artist < ActiveRecord::Base
   has_many :gigs
   has_many :venues, through: :gigs
+
+  def self.search(q)
+    where('name like :value OR description like :value', value: "%#{q}%")
+  end
+  
 end
