472f8dfa757ef6c16ccff8a2825a037ba65f444f
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-11 14:52:02 +0000

    added create gig

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -4,8 +4,22 @@ class GigsController < ApplicationController
     @gigs = Gig.all
   end
 
+  def new
+    @gig = Gig.new
+  end
+
+  def create
+    Gig.create(gig_params)
+    redirect_to(gigs_path)
+  end
+
   def show
     @gig = Gig.find(params[:id])
   end
+
+  private
+    def gig_params
+      params.require(:gig).permit(:name, :description, :artist_id, :venue_id, :ticket_price, :tickets_available, :start_time, :end_time)
+    end
 
 end
