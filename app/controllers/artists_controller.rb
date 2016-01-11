a874f815c9a8201f423df1d59fd610c84eea2dd4
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-11 13:56:26 +0000

    added create artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,8 +4,22 @@ class ArtistsController < ApplicationController
       @artists = Artist.all
     end
 
+    def new
+      @artist = Artist.new
+    end
+
+    def create
+      Artist.create(artist_params)
+      redirect_to(artists_path)
+    end
+
     def show
       @artist = Artist.find(params[:id])
     end
+
+    private
+      def artist_params
+        params.require(:artist).permit(:name, :description, :image)
+      end
 
 end
