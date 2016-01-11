beb70514f1b25301db35f603aa5aba11327dc898
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-11 14:06:44 +0000

    added create venue

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -4,8 +4,22 @@ class VenuesController < ApplicationController
     @venues = Venue.all
   end
 
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    Venue.create(venue_params)
+    redirect_to(venues_path)
+  end
+
   def show
     @venue = Venue.find(params[:id])
   end
+
+  private
+    def venue_params
+      params.require(:venue).permit(:name, :location, :image)
+    end
 
 end
