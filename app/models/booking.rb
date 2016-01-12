df0c5cdc4753b47c0af8595749843a722937138d
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-12 15:14:33 +0000

    schedule clash checker added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -8,9 +8,13 @@ class Booking < ActiveRecord::Base
     current_user.bookings.each do |b|
       existing_starttime = DateTime.parse(b.gig.start_time)
       existing_endtime = DateTime.parse(b.gig.end_time)
-      raise
+      if (new_starttime >= existing_starttime and new_starttime <= existing_endtime) or (new_endtime >= existing_starttime and new_endtime <= existing_endtime)
+        return true
+      elsif (new_starttime <= existing_starttime) and (new_endtime >= existing_endtime)
+        return true
+      else return false
+      end
     end
-
   end
 
 end
