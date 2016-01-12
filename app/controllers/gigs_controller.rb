dfc1ccace94b660f5ccc8ddb077e2fbe6d8493e6
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-12 16:44:37 +0000

    added edit for gigs

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -17,6 +17,16 @@ class GigsController < ApplicationController
     @gig = Gig.find(params[:id])
   end
 
+  def edit
+    @gig = Gig.find(params[:id])
+  end
+
+  def update
+    @gig = Gig.find(params[:id])
+    @gig.update(gig_params)
+    redirect_to(gigs_path)
+  end
+
   private
     def gig_params
       params.require(:gig).permit(:name, :description, :artist_id, :venue_id, :ticket_price, :tickets_available, :start_time, :end_time, :keywords)
