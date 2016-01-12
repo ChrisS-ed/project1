31cc3e02b1d3390eed4b696b3574d040e32d33db
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-12 16:31:02 +0000

    added edit for artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -17,6 +17,16 @@ class ArtistsController < ApplicationController
       @artist = Artist.find(params[:id])
     end
 
+    def edit
+      @artist = Artist.find(params[:id])
+    end
+
+    def update
+      @artist = Artist.find(params[:id])
+      @artist.update(artist_params)
+      redirect_to(artists_path)
+    end
+
     private
       def artist_params
         params.require(:artist).permit(:name, :description, :image)
