f922e70aa721a545f5cf7eae790a274466617764
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-13 11:03:49 +0000

    stop user from booking gig they already have seats for

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,9 +14,17 @@ class BookingsController < ApplicationController
     seats_wanted = booking_params[:seats].to_i
     this_gig = Gig.find(booking_params[:gig_id])
 
+    # CHECK IF USER HAS ALREADY BOOKED THIS GIG
+    existing_seats_held = current_user.bookings.find_by(gig_id: this_gig.id)
+    if existing_seats_held
+      flash[:alert] = "BOOKING FAILED: Sorry, you have already booked this gig"
+      redirect_to(new_gig_booking_path(this_gig))
+      return
+    end
+
     # CHECK FOR GIG SCHEDULE CLASHES
     if Booking.schedule_clash(this_gig, current_user)
-      flash[:alert] = "BOOKING FAILED: Sorry, this gig clashes with one of your existing bookings"
+      flash[:alert] = "BOOKING FAILED: Sorry, this gig clashes with one of your other bookings"
       redirect_to(new_gig_booking_path(this_gig))
       return
     end
