5ce92555c505cf52776ea49aa95dd4063d18dd45
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-13 10:32:19 +0000

    shows user if they have already booked seats for show

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -9,6 +9,7 @@ class Booking < ActiveRecord::Base
     new_starttime = DateTime.parse(new_gig.start_time)
     new_endtime = DateTime.parse(new_gig.end_time)
     current_user.bookings.each do |b|
+      # raise
       existing_starttime = DateTime.parse(b.gig.start_time)
       existing_endtime = DateTime.parse(b.gig.end_time)
       if (new_starttime >= existing_starttime and new_starttime <= existing_endtime) or (new_endtime >= existing_starttime and new_endtime <= existing_endtime)
